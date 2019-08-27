2142440069c4d70a20c9db6efac8af9661ef5067
Burak Bayramli <burak@localhost.com>
2019-08-27 14:12:29 +0300

    -

---
+++ algs/build.py
@@ -0,0 +1,67 @@
+import os, sys, glob, shutil
+d = os.environ['HOME']
+if len(sys.argv) == 1:
+    cmd = "pdfunite ./cover/cover.pdf \
+    ./mlintro/mlintro.pdf \
+    ./complexity/complexity.pdf \
+    ./basic/basic.pdf \
+    ./sort/sort.pdf \
+    ./dynp/dynp.pdf  \
+    ./minspan/minspan.pdf \
+    ./dijks/dijks.pdf \
+    ./mstseg/mstseg.pdf \
+    ./probsolve/probsolve.pdf \
+    ./mcts/mcts.pdf \
+    ./go/go.pdf \
+    ./huff/huff.pdf \
+    ./id3/id3.pdf \
+    ./knn/knn.pdf \
+    ./enc/enc.pdf \
+    ./kmeans/kmeans.pdf \
+    ./kmeans_mr/kmeans_mr.pdf \
+    ./meanshift/meanshift.pdf \
+    ./modeval/modeval.pdf \
+    ./crf/crf.pdf \
+    ./mesquita/mesquita.pdf \
+    ./graph/graph.pdf \
+    ./rsa/rsa.pdf \
+    ./street/street.pdf \
+    ./mlp/mlp.pdf \
+    ./autodiff/autodiff.pdf \
+    ./tensorflow/tensorflow.pdf \
+    ./convnet/convnet.pdf \
+    ./rnn/rnn.pdf \
+    ./lstm/lstm.pdf \
+    ./autoenc/autoenc.pdf \
+    ./reinf/reinf.pdf \
+    ./nlp/nlp.pdf \
+    ./recom/recom.pdf \
+    ./nmt/nmt.pdf \
+    ./ocr/ocr.pdf \
+    ./speech/speech.pdf \
+    ./graphconv/graphconv.pdf \
+    ./gan/gan.pdf \
+    ./zapp/zapp.pdf \
+    " + d + "/Downloads/algs.pdf"
+    os.system(cmd)
+    print (cmd)
+    exit()
+elif sys.argv[1] == 'all':
+    print (os.listdir("."))
+    for a in os.listdir("."):
+        if os.path.isdir(a):
+            os.chdir(a)
+            os.system("pdflatex -shell-escape *.tex")    
+            os.chdir("..")
+               
+elif sys.argv[1] == 'clean':
+    os.system("find . -name '_region_*' | xargs rm  -rf")
+
+elif sys.argv[1] == 'tex':
+    file = glob.glob('*.tex')
+    os.system("pdflatex -shell-escape %s" % file[0])
+    d = "/data/data/com.termux/files/home/storage/downloads"
+    if os.path.isdir(d):
+        ff = file[0].replace(".tex",".pdf")
+        shutil.copy(ff,d)
+        
---
+++ phy/build.py
@@ -0,0 +1,38 @@
+import os, sys, glob, shutil
+
+d = os.environ['HOME']
+if len(sys.argv) == 1 :
+    os.system("pdfunite \
+    phy_cover/phy_cover.pdf \
+    phy_basics/phy_basics.pdf \
+    phy_lagrange/phy_lagrange.pdf \
+    phy_dblpend/phy_dblpend.pdf \
+    phy_cartpole/phy_cartpole.pdf \
+    phy_under/phy_under.pdf \
+    phy_varcalc/phy_varcalc.pdf \
+    phy_opt/phy_opt.pdf \
+    phy_path/phy_path.pdf \
+    phy_kf/phy_kf.pdf \
+    " + d + "/Downloads/phy.pdf" \
+    )
+    exit()
+    
+elif sys.argv[1] == 'all':
+    for a in sorted(glob.glob("phy*")):
+        os.chdir(a)
+        os.system("pdflatex -shell-escape %s" % glob.glob("phy_*.tex")[0] )
+        os.chdir("..")
+        
+    
+elif sys.argv[1] == 'clean':
+    os.system("find . -name '_region_*' | xargs rm  -rf")
+    os.system("find . -name '_minted-*' | xargs rm  -rf")
+            
+elif sys.argv[1] == 'tex':
+    file = glob.glob('phy_*.tex')
+    os.system("pdflatex -shell-escape %s" % file[0])
+    d = "/data/data/com.termux/files/home/storage/downloads"
+    if os.path.isdir(d):
+        ff = file[0].replace(".tex",".pdf")
+        shutil.copy(ff,d)
+    
