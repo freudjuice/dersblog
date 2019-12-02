04bfda1b2a0a30345af20914c0d599303634267e
BB <burak@localhost.com>
2019-12-02 16:13:37 +0300

    -

--- func_analysis/func_50_bfgs/bfgs.py
+++ func_analysis/func_50_bfgs/bfgs.py
@@ -1,12 +1,7 @@
 from scipy.optimize import minpack2
 import warnings
-import numpy
 import numpy as np
 from scipy._lib.six import xrange
-import numpy.linalg as lin
-import scipy.linalg as slin
-from numpy import (atleast_1d, eye, mgrid, argmin, zeros, shape, squeeze,
-                   asarray, sqrt, Inf, asfarray, isinf)
 
 _status_message = {'success': 'Optimization terminated successfully.',
                    'maxfev': 'Maximum number of function evaluations has '
@@ -51,15 +46,7 @@ def wrap_function(function, args):
 
     return ncalls, function_wrapper
 
-_epsilon = sqrt(numpy.finfo(float).eps)
-
-class OptimizeWarning(UserWarning):
-    pass
-
-class _LineSearchError(RuntimeError):
-    pass
-
-#from scipy.optimize import (minimize)
+_epsilon = np.sqrt(np.finfo(float).eps)
         
 def approx_fprime(xk, f, epsilon, *args):
     return _approx_fprime_helper(xk, f, epsilon, args=args)
@@ -67,8 +54,8 @@ def approx_fprime(xk, f, epsilon, *args):
 def _approx_fprime_helper(xk, f, epsilon, args=(), f0=None):
     if f0 is None:
         f0 = f(*((xk,) + args))
-    grad = numpy.zeros((len(xk),), float)
-    ei = numpy.zeros((len(xk),), float)
+    grad = np.zeros((len(xk),), float)
+    ei = np.zeros((len(xk),), float)
     for k in range(len(xk)):
         ei[k] = 1.0
         d = epsilon * ei
@@ -84,29 +71,15 @@ def _approx_fprime_helper(xk, f, epsilon, args=(), f0=None):
         ei[k] = 0.0
     return grad
 
-def vecnorm(x, ord=2):
-    if ord == Inf:
-        return numpy.amax(numpy.abs(x))
-    elif ord == -Inf:
-        return numpy.amin(numpy.abs(x))
-    else:
-        return numpy.sum(numpy.abs(x)**ord, axis=0)**(1.0 / ord)
+def vecnorm(x):
+    return np.amax(np.abs(x))
 
 def line_search_wolfe1(f, fprime, xk, pk, gfk=None,
                        old_fval=None, old_old_fval=None,
                        args=(), c1=1e-4, c2=0.9, amax=50, amin=1e-8,
                        xtol=1e-14):
-    if gfk is None:
-        gfk = fprime(xk)
-
-    if isinstance(fprime, tuple):
-        eps = fprime[1]
-        fprime = fprime[0]
-        newargs = (f, eps) + args
-        gradient = False
-    else:
-        newargs = args
-        gradient = True
+    newargs = args
+    gradient = True
 
     gval = [gfk]
     gc = [0]
@@ -133,82 +106,9 @@ def line_search_wolfe1(f, fprime, xk, pk, gfk=None,
     return stp, fc[0], gc[0], fval, old_fval, gval[0]
 
     
-def _line_search_wolfe12(f, fprime, xk, pk, gfk, old_fval, old_old_fval,
-                         **kwargs):
-    extra_condition = kwargs.pop('extra_condition', None)
-
-    ret = line_search_wolfe1(f, fprime, xk, pk, gfk,
-                             old_fval, old_old_fval,
-                             **kwargs)
-
-    if ret[0] is not None and extra_condition is not None:
-        xp1 = xk + ret[0] * pk
-        if not extra_condition(ret[0], xp1, ret[3], ret[5]):
-            # Reject step if extra_condition fails
-            ret = (None,)
-
-    if ret[0] is None:
-        # line search failed: try different one.
-        with warnings.catch_warnings():
-            warnings.simplefilter('ignore', LineSearchWarning)
-            kwargs2 = {}
-            for key in ('c1', 'c2', 'amax'):
-                if key in kwargs:
-                    kwargs2[key] = kwargs[key]
-            ret = line_search_wolfe2(f, fprime, xk, pk, gfk,
-                                     old_fval, old_old_fval,
-                                     extra_condition=extra_condition,
-                                     **kwargs2)
-
-    if ret[0] is None:
-        raise _LineSearchError()
-
-    return ret
-
-
 def scalar_search_wolfe1(phi, derphi, phi0=None, old_phi0=None, derphi0=None,
                          c1=1e-4, c2=0.9,
                          amax=50, amin=1e-8, xtol=1e-14):
-    """
-    Scalar function search for alpha that satisfies strong Wolfe conditions
-
-    alpha > 0 is assumed to be a descent direction.
-
-    Parameters
-    ----------
-    phi : callable phi(alpha)
-        Function at point `alpha`
-    derphi : callable phi'(alpha)
-        Objective function derivative. Returns a scalar.
-    phi0 : float, optional
-        Value of phi at 0
-    old_phi0 : float, optional
-        Value of phi at previous point
-    derphi0 : float, optional
-        Value derphi at 0
-    c1 : float, optional
-        Parameter for Armijo condition rule.
-    c2 : float, optional
-        Parameter for curvature condition rule.
-    amax, amin : float, optional
-        Maximum and minimum step size
-    xtol : float, optional
-        Relative tolerance for an acceptable step.
-
-    Returns
-    -------
-    alpha : float
-        Step size, or None if no suitable step was found
-    phi : float
-        Value of `phi` at the new point `alpha`
-    phi0 : float
-        Value of `phi` at `alpha=0`
-
-    Notes
-    -----
-    Uses routine DCSRCH from MINPACK.
-
-    """
 
     if phi0 is None:
         phi0 = phi(0.)
@@ -249,7 +149,7 @@ def scalar_search_wolfe1(phi, derphi, phi0=None, old_phi0=None, derphi0=None,
     return stp, phi1, phi0
 
 def _minimize_bfgs(fun, x0, args=(), jac=None, callback=None,
-                   gtol=1e-5, norm=Inf, eps=_epsilon, maxiter=None,
+                   gtol=1e-5, norm=np.Inf, eps=_epsilon, maxiter=None,
                    disp=False, return_all=False,
                    **unknown_options):
 
@@ -258,23 +158,18 @@ def _minimize_bfgs(fun, x0, args=(), jac=None, callback=None,
     epsilon = eps
     retall = return_all
 
-    x0 = asarray(x0).flatten()
-    if x0.ndim == 0:
-        x0.shape = (1,)
-    if maxiter is None:
-        maxiter = len(x0) * 200
+    x0 = np.asarray(x0).flatten()
+
     func_calls, f = wrap_function(f, args)
 
     old_fval = f(x0)
 
-    if fprime is None:
-        grad_calls, myfprime = wrap_function(approx_fprime, (f, epsilon))
-    else:
-        grad_calls, myfprime = wrap_function(fprime, args)
+    grad_calls, myfprime = wrap_function(approx_fprime, (f, epsilon))
+
     gfk = myfprime(x0)
     k = 0
     N = len(x0)
-    I = numpy.eye(N, dtype=int)
+    I = np.eye(N, dtype=int)
     Hk = I
 
     # Sets the initial step guess to dx ~ 1
@@ -284,12 +179,12 @@ def _minimize_bfgs(fun, x0, args=(), jac=None, callback=None,
     if retall:
         allvecs = [x0]
     warnflag = 0
-    gnorm = vecnorm(gfk, ord=norm)
+    gnorm = vecnorm(gfk)
     while (gnorm > gtol) and (k < maxiter):
-        pk = -numpy.dot(Hk, gfk)
+        pk = -np.dot(Hk, gfk)
         try:
             alpha_k, fc, gc, old_fval, old_old_fval, gfkp1 = \
-                     _line_search_wolfe12(f, myfprime, xk, pk, gfk,
+                     line_search_wolfe1(f, myfprime, xk, pk, gfk,
                                           old_fval, old_old_fval, amin=1e-100, amax=1e100)
         except _LineSearchError:
             # Line search failed to find a better solution.
@@ -297,62 +192,39 @@ def _minimize_bfgs(fun, x0, args=(), jac=None, callback=None,
             break
 
         xkp1 = xk + alpha_k * pk
-        if retall:
-            allvecs.append(xkp1)
+
         sk = xkp1 - xk
         xk = xkp1
-        if gfkp1 is None:
-            gfkp1 = myfprime(xkp1)
 
         yk = gfkp1 - gfk
         gfk = gfkp1
-        if callback is not None:
-            callback(xk)
         k += 1
-        gnorm = vecnorm(gfk, ord=norm)
-        if (gnorm <= gtol):
-            break
+        gnorm = vecnorm(gfk)
+        if (gnorm <= gtol): break
 
-        if not numpy.isfinite(old_fval):
+        if not np.isfinite(old_fval):           
             # We correctly found +-Inf as optimal value, or something went
             # wrong.
             warnflag = 2
             break
 
         try:  # this was handled in numeric, let it remaines for more safety
-            rhok = 1.0 / (numpy.dot(yk, sk))
+            rhok = 1.0 / (np.dot(yk, sk))
         except ZeroDivisionError:
             rhok = 1000.0
             if disp:
                 print("Divide-by-zero encountered: rhok assumed large")
-        if isinf(rhok):  # this is patch for numpy
+        if np.isinf(rhok):  # this is patch for numpy
             rhok = 1000.0
             if disp:
                 print("Divide-by-zero encountered: rhok assumed large")
-        A1 = I - sk[:, numpy.newaxis] * yk[numpy.newaxis, :] * rhok
-        A2 = I - yk[:, numpy.newaxis] * sk[numpy.newaxis, :] * rhok
-        Hk = numpy.dot(A1, numpy.dot(Hk, A2)) + (rhok * sk[:, numpy.newaxis] *
-                                                 sk[numpy.newaxis, :])
+        A1 = I - sk[:, np.newaxis] * yk[np.newaxis, :] * rhok
+        A2 = I - yk[:, np.newaxis] * sk[np.newaxis, :] * rhok
+        Hk = np.dot(A1, np.dot(Hk, A2)) + (rhok * sk[:, np.newaxis] * sk[np.newaxis, :])
 
     fval = old_fval
 
-    if warnflag == 2:
-        msg = _status_message['pr_loss']
-    elif k >= maxiter:
-        warnflag = 1
-        msg = _status_message['maxiter']
-    elif np.isnan(gnorm) or np.isnan(fval) or np.isnan(xk).any():
-        warnflag = 3
-        msg = _status_message['nan']
-    else:
-        msg = _status_message['success']
-
-    if disp:
-        print("%s%s" % ("Warning: " if warnflag != 0 else "", msg))
-        print("         Current function value: %f" % fval)
-        print("         Iterations: %d" % k)
-        print("         Function evaluations: %d" % func_calls[0])
-        print("         Gradient evaluations: %d" % grad_calls[0])
+    msg = _status_message['success']
 
     result = OptimizeResult(fun=fval, jac=gfk, hess_inv=Hk, nfev=func_calls[0],
                             njev=grad_calls[0], status=warnflag,
@@ -362,30 +234,21 @@ def _minimize_bfgs(fun, x0, args=(), jac=None, callback=None,
         result['allvecs'] = allvecs
     return result
 
-def minimize(fun, x0, args=(), method=None, jac=None, hess=None,
+def minimize(fun, x0, args=(), hess=None,
              hessp=None, bounds=None, constraints=(), tol=None,
              callback=None, options=None):
     
-    x0 = np.asarray(x0)
-    if x0.dtype.kind in np.typecodes["AllInteger"]:
-        x0 = np.asarray(x0, dtype=float)
-
-    if not isinstance(args, tuple):
-        args = (args,)
-
     jac = None
-
-    return _minimize_bfgs(fun, x0, args, jac, callback, **options)
+    return _minimize_bfgs(fun, np.asarray(x0), args, jac, callback, **options)
 
 def rosenbrock(x):
     return (1 - x[0])**2 + 100*(x[1] - x[0]**2)**2
 
 x0 = [-1.0,0]
 
-opts = {'maxiter': 1000, 'verbose': 2}
+opts = {'maxiter': 1000}
 res = minimize (fun=rosenbrock,
                 x0=x0,
-                method = 'BFGS',
                 options=opts)
 
 print (res)
