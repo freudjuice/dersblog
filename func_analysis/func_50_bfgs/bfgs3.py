dd339f410c03b41aa2e4158ba96e55b8a7001aa4
BB <burak@localhost.com>
2019-12-09 14:02:12 +0300

    -

--- func_analysis/func_50_bfgs/bfgs3.py
+++ func_analysis/func_50_bfgs/bfgs3.py
@@ -1,60 +1,100 @@
-from numpy import *
-from math import *
-from numpy.linalg import *
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy.linalg as lin
 
-testfn_vectorspace = lambda x: (1-x[0])**2 + 100*(x[1]-x[0]**2)**2
+eps = np.sqrt(np.finfo(float).eps)
 
-def testfn_d(x):
-        return array([2*100*(x[1] - x[0]**2)*(-2*x[0]) - 2*(1.-x[0]), 2*100*(x[1]-x[0]**2)])
+def _approx_fprime_helper(xk, f, epsilon):
+    f0 = f(xk)
+    grad = np.zeros((len(xk),), float)
+    ei = np.zeros((len(xk),), float)
+    for k in range(len(xk)):
+        ei[k] = 1.0
+        d = epsilon * ei
+        df = (f(xk + d) - f0) / d[k]
+        if not np.isscalar(df):
+            try:
+                df = df.item()
+            except (ValueError, AttributeError):
+                raise ValueError("The user-provided "
+                                 "objective function must "
+                                 "return a scalar value.")
+        grad[k] = df
+        ei[k] = 0.0
+    return grad
 
-def Newton_BFGS(F,Fprime, Start, epsi = 10e-8, tol = 10e-6, sigma= 10**-1, beta = 10 ): 
-        def LineSearch(g,x,s,sigma= 10**-1, beta = 10, convergval = 0.00001):                
-                def QFind(alpha):
-                        if abs(alpha) < convergval:
-                                return 1
-                        return (F(x + alpha*s) - F(x))/(alpha * dot(g,s))
+def rosen(x):
+    return 100*(x[1]-x[0]**2)**2+(1-x[0])**2
 
-                
-                alpha = 1.
+def rosen_real(x):
+    gy =[-400*(x[1]-x[0]**2)*x[0]-2*(1-x[0]), 200*(x[1]-x[0]**2)]
+    return rosen(x), gy
 
-                # Double alpha until big enough
-                while QFind(alpha) >= sigma:
-                        alpha=alpha * 2
+def rosen_approx(x):
+    g = _approx_fprime_helper(x, rosen, eps)
+    return rosen(x),g
 
-                # BTacking
-                while QFind(alpha)< sigma:
-                        alphap=alpha / ( 2.0* ( 1- QFind(alpha)))
-                        alpha=max(1.0/beta * alpha, alphap)
-                return alpha
+def linesearch_secant(f, d, x):
+    epsilon=10**(-5)
+    max = 500
+    alpha_curr=0
+    alpha=10**-5
+    y,grad=f(x)
+    dphi_zero=np.dot(np.array(grad).T,d)
 
-        # Startup
-        x = Start
-        xold = inf
-        N = shape(x)[0]
-        H = 1.0 * eye(N)
-        counter = 1
-        alpha = 1
-        g = Fprime(x)
-        while norm(g) > epsi and norm(xold - x) > tol:
-                s = -dot(H,g)
-                # Repeating the linesearch
-                alpha = LineSearch(g,x,s)
-                x = x+alpha*s
-                gold = g
-                g = Fprime(x)
-                y = (g - gold)/alpha
-                dotsy = dot(s,y)
-                if dotsy>0:
-                        # Update H using estimation technique
-                        z = dot(H,y)
-                        H += outer(s,s)*(dot(s,y) + dot(y, z))/dotsy**2 - (outer(z,s)+ outer(s, z))/dotsy
-                # Implement Counter 
-                counter+=1
+    dphi_curr=dphi_zero
+    i=0;
+    while np.abs(dphi_curr)>epsilon*np.abs(dphi_zero):
+        alpha_old=alpha_curr
+        alpha_curr=alpha
+        dphi_old=dphi_curr
+        y,grad=f(x+alpha_curr*d)
+        dphi_curr=np.dot(np.array(grad).T,d)
+        alpha=(dphi_curr*alpha_old-dphi_old*alpha_curr)/(dphi_curr-dphi_old);
+        i += 1
+        if (i >= max) and (np.abs(dphi_curr)>epsilon*np.abs(dphi_zero)):
+            print('Line search terminating with number of iterations:')
+            print(i)
+            print(alpha)
+            break
+        
+    return alpha
 
-        return (x , counter)
+def bfgs(x, func):
+    
+    H = np.eye(2)
+    tol = 1e-20
+    y,grad = func(x)
+    dist=2*tol
+    epsilon = tol
+    iter=0;
 
-(result, counter) =  Newton_BFGS(testfn_vectorspace, testfn_d, array([-1,0]))
+    while lin.norm(grad)>1e-6:
+        value,grad=func(x)
+        p=np.dot(-H,grad)
+        lam = linesearch_secant(func,p,x)
+        iter += 1
+        xt = x
+        x = x + lam*p
+        s = lam*p
+        dist=lin.norm(s)
+        newvalue,newgrad=func(x)
+        y = np.array(newgrad)-grad
+        rho=1/np.dot(y.T,s)
+        s = s.reshape(2,1)
+        y = y.reshape(2,1)
+        tmp1 = np.eye(2)-rho*np.dot(s,y.T)
+        tmp2 = np.eye(2)-rho*np.dot(y,s.T)
+        tmp3 = rho*np.dot(s,s.T)
+        H= np.dot(np.dot(tmp1,H),tmp2) + tmp3
+        #print ('lambda:',lam)
 
-print ("Custom BFGS:")
-print ("Final Result: " + str(result))
-print ("Iteration Count: " + str(counter))
+    print (xt)
+    print ('iter',iter)
+
+
+x=np.array([-1.0,0])
+bfgs(x,rosen_approx)
+#bfgs(x,rosen_real)
+    
