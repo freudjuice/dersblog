d507a008f7da20671ad331501c5dd14535336536
BB <burak@localhost.com>
2020-02-27 23:33:30 +0300

    -

--- func_analysis/func_60_primdual/primdual.m
+++ func_analysis/func_60_primdual/primdual.m
@@ -11,8 +11,10 @@ x = zeros(n,1);
 
 b = ones(n,1)*10.;
 q = ones(n,1)*3.;
-A = rand(n,n)
-P = rand(n,n)
+A = [2 3 5; 3 4 5; 4 5 3];
+%A = rand(n,n);
+%P = rand(n,n)
+P = [1 2 4; 2 4 4; 1 1 1];
 
 s = b-A*x;
 z = 1./s;
@@ -22,10 +24,15 @@ for iters = 1:MAXITERS
   if ((gap < TOL) && (norm(res) < RESTOL)), break; end;
   tinv = gap/(m*MU);
   sol = -[ P A'; A diag(-s./z) ] \ [ P*x+q+A'*z; -s + tinv*(1./z) ];
-  dx = sol(1:n); dz = sol(n+[1:m]); ds = -A*dx;
+  dx = sol(1:n);
+  dz = sol(n+[1:m]);
+  ds = -A*dx;
   r = [P*x+q+A'*z; z.*s-tinv];
   step = min(1.0, 0.99/max(-dz./z));
-  while (min(s+step*ds) <= 0), step = BETA*step; end;
+  while (min(s+step*ds) <= 0),
+    step = BETA*step;
+    step
+  end;
   newz = z+step*dz; newx = x+step*dx; news = s+step*ds;
   newr = [P*newx+q+A'*newz; newz.*news-tinv];
   while (norm(newr) > (1-ALPHA*step)*norm(r))
@@ -34,5 +41,6 @@ for iters = 1:MAXITERS
     newr = [P*newx+q+A'*newz; newz.*news-tinv];
   end;
   x = x+step*dx; z = z +step*dz; s = b-A*x;
+
 end;
 x
