ba1bd1eb616770e390a6788f140b944982a7dd4f
BB <burak@localhost.com>
2020-03-11 12:11:34 +0300

    -

--- func_analysis/func_42_subgrad/subgrad_func.m
+++ func_analysis/func_42_subgrad/subgrad_func.m
@@ -2,18 +2,16 @@
 % 0.5*||Ax - b||_2 + lambda*||x||_1
 % subgradient method
 function subgrad_func(A,b,lambda)
-[~,n2] = size(A);
-x = zeros(n2,1);
-k=1;
-g = ones(n2,1);
-t = 0.0001;
+  [~,n2] = size(A);
+  x = zeros(n2,1);
+  k=1;
+  g = ones(n2,1);
+  t = 0.01;
 
-t_lapse(1)=0;
-
-while k<3 || (abs(f(k-1)-f(k-2))/f(k-1))>1e-5
-    tic
+  while k<3 || abs(f(k-1)-f(k-2))/f(k-1)>1e-5
     % f(round(k/10)+1)=0.5*norm(A*x-b,2)^2+lambda*norm(x,1);
     f(k)=0.5*norm(A*x-b,2)^2+lambda*norm(x,1);
+    disp(f(k));
     % the subgradient is A'*(A*x-b)
     s = x;
     s(x>0)=1;
@@ -21,7 +19,6 @@ while k<3 || (abs(f(k-1)-f(k-2))/f(k-1))>1e-5
     s(x==0) = -2*rand(length(find(x==0)),1)+1;
     g = A'*(A*x-b)+lambda*s;
     x = x - t*g;
-    t_lapse(k)=toc;
     k = k+1;
 end;
 x
--- func_analysis/func_42_subgrad/tst1.m
+++ func_analysis/func_42_subgrad/tst1.m
@@ -4,9 +4,12 @@ n = 30; p = 30; sparse_ratio = 0.01;
 A = randn(n,p);
 beta = randn(p,1);
 
-beta(randperm(p,round((1-sparse_ratio)*p)))=0;beta0 = randn;
+beta(randperm(p,round((1-sparse_ratio)*p)))=0;
+beta0 = randn;
 b = beta0 + A*beta + randn(n,1);
 
 lambda = 0.1;
+
+beta
 
 subgrad_func(A,b,lambda);
